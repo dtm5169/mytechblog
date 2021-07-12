6a1d14b65277305fbcd8b8f42491c6694c07b24b
Daisha Marrow <marrowdaisha@gmail.com>
2021-07-12 18:39:39 -0400

    final push

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PW,
   {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  }
+  },
 );
 
 module.exports = sequelize;
